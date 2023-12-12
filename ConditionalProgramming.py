cf7a5f2c0db16ae1a332889c5d98861908866025
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-12-12 11:20:40 -0600

    Creating example for show behavior using conditional programming

---
+++ ConditionalProgramming.py
@@ -0,0 +1,14 @@
+# Programación condicional en Python
+
+# Ejemplo: Verificar si un número es positivo, negativo o cero
+
+# Obtener un número del usuario
+numero = float(input("Ingrese un número: "))
+
+# Uso de la declaración 'if' para realizar la verificación
+if numero > 0:
+    print("El número es positivo.")
+elif numero < 0:
+    print("El número es negativo.")
+else:
+    print("El número es cero.")
