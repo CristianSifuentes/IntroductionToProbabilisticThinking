9dc992ae3fe6c7612a4f7375164ad547dc7d82f7
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-12-13 15:36:15 -0600

    Adding information

---
+++ TexasSniperFallacy.py
@@ -0,0 +1,9 @@
+'''
+La falacia del francotirador de Texas, en el contexto de la probabilidad y la estadística, se refiere a la tendencia de buscar patrones significativos en conjuntos de datos después de que se han recopilado, en lugar de establecer hipótesis a priori y recopilar datos de acuerdo con esas hipótesis.
+
+En términos de probabilidad, esta falacia destaca la importancia de tener cuidado al interpretar patrones o tendencias en los datos, especialmente cuando hay múltiples posibilidades de encontrar algún patrón simplemente por azar.
+
+Un ejemplo podría ser el siguiente: Supongamos que alguien revisa los registros de desempeño de los estudiantes en un examen y encuentra un pequeño grupo de estudiantes que tuvieron un rendimiento excepcional. Luego, esa persona decide analizar en detalle ese grupo en busca de cualquier patrón o característica común, como el color de su ropa, lo que comieron antes del examen, etc. Si encuentra algún patrón significativo, podría cometer la falacia del francotirador de Texas si no tenía una hipótesis clara antes de analizar los datos.
+
+En resumen, en probabilidad y estadística, es esencial establecer hipótesis a priori y realizar análisis de datos de manera rigurosa para evitar caer en la falacia del francotirador de Texas y obtener interpretaciones sesgadas o incorrectas.
+'''
